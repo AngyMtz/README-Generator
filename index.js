11d382b19d7ca8e4bff3e3f126b7560a05b78005
Norma Martinez <nmartinezlagarda@gmail.com>
2021-05-08 21:02:39 -0700

    license, links and descripcions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 inquirer
   .prompt([
@@ -6,47 +7,56 @@ inquirer
       type: 'input',
       message: 'What is your GitHub username?',
       name: 'gitName',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'What is your e-mail address?',
       name: 'email',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'What is your Project name?',
       name: 'projName',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'Short description of your project?',
       name: 'Descrip',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'What problem does it solve?',
       name: 'probSolv',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'What command should be run to install dependencies?',
       name: 'install',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'list',
       name: 'license',
       message: 'What type of license did you use for your project?',
       choices: ['MIT', 'Apache 2.0', 'GNU', 'BSD3', 'None'],
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'Who collaborated on this project?',
       name: 'collabor',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
     {
       type: 'input',
       message: 'What does the user need to know about using the repo??',
       name: 'userInst',
+      validate: (value)=>{ if(value){return true} else {return 'I need a value to continue'}},
     },
   ])
   .then((response) =>
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license){
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    case 'GNU':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    case 'BSD3':
+      return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    case 'None':
+      return ' '
+    default:
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license){
+    case 'MIT':
+      return 'https://choosealicense.com/licenses/mit/'
+    case 'Apache 2.0':
+      return 'https://choosealicense.com/licenses/'
+    case 'GNU':
+      return 'https://choosealicense.com/licenses/gpl-3.0/'
+    case 'BSD3':
+      return 'https://choosealicense.com/licenses/bsd-3-clause/'
+    case 'None':
+      return ' '
+    default:
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch(license){
+    case 'MIT':
+      return 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code. '
+    case 'Apache 2.0':
+      return 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code. '
+    case 'GNU':
+      return 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'
+    case 'BSD3':
+      return 'This license has also been called the "New BSD License" or "Modified BSD License". Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.'
+    case 'None':
+      return ' '
+    default:
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
